e0971f2dc1c81028ad102ef1d777e434520cc676
guxiaojuan <1634747911@qq.com>
2016-04-12 23:07:44 +0800

    哇哈哈

---
+++ ife19/ife19.js
@@ -0,0 +1,70 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta http-equiv="content-type" content="text/html;charset=utf-8">
+   <title>队列模拟</title>
+   <style>
+     li{
+	   list-style:none;
+	   display:inline-block;
+	   color:#FFFFFF;
+	   background:#FF0000;
+	   width:15px;
+	   border:1px solid #999999;
+	 }
+   </style>
+   
+</head>
+
+<body>
+ <div>
+   <input type="text" id="txt">
+   <input type="button" value="左侧入" name="btn">
+   <input type="button" value="右侧入" name="btn">
+   <input type="button" value="左侧出" name="btn">
+   <input type="button" value="右侧出" name="btn">
+   <input type="button" value="随机" name="btn">
+   <input type="button" value="排序" name="btn">
+ </div>
+
+ <div id="sor_t">
+   <ul></ul>
+ </div>
+ 
+  <script src="ife19.js" charset="gb2312"></script>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
