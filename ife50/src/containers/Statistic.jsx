c5e769be372eb558516e3265b101ddfe8a74b59d
guxiaojuan <guxiaojuan@500wan.com>
2018-06-15 18:01:41 +0800

    '路由部分搞好啦彻底'

--- ife50/src/containers/Statistic.jsx
+++ ife50/src/containers/Statistic.jsx
@@ -1,2 +1,18 @@
 import React from 'react'
 
+export default class Statistic extends React.Component {
+	constructor(props) {
+		super(props);
+	};
+
+	componentDidMount () {
+		console.log('---------路由--------')
+		console.log(this.props)
+	}
+
+	render () {
+		return (
+			<div  style={{marginTop: '10rem'}}>我的问卷部分</div>
+		)
+	}
+}
